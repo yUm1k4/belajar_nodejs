d226cce81ca433d254cc36ce525f126575d0b830
yUm1k4 <zaiabdullah99@gmail.com>
2021-05-05 15:01:14 +0700

    update #13. NodeJS Web Server

--- webServer/app.js
+++ webServer/app.js
@@ -3,32 +3,49 @@ const http = require('http')
 const fs = require('fs')
 const port = 3000
 
+const renderHTML = (path, res) => {
+    // asyc ada callback err dan data
+    fs.readFile(path, (err, data) => {
+        // jika error
+        if (err) {
+            // tulis head 404 not found
+            res.writeHead(404);
+            res.write('Error: File Not Found!');
+        } else { // jika berhasil
+            // cetak isi file nya
+            res.write(data);
+        }
+        res.end();
+    });   
+}
+
 http.createServer((req, res) => {
     res.writeHead(200, {
         'Content-Type': 'text/html'
     });
 
     const url = req.url;
-    if (url === '/about') {
-        res.write('<h1>Ini adalah halaman about</h1>');
-        res.end();
-    } else if (url === '/contact') {
-        res.write('<h1>Ini adalah halaman contact</h1>');
-        res.end();
-    } else {
-        // res.write('<h1>Hello World!</h1>')
-        fs.readFile('./index.html', (err, data) => {
-            if (err) {
-                res.writeHead(404);
-                res.write('Error: File Not Found!');
-            } else {
-                res.write(data);
-            }
-            res.end();
-        });
+
+    switch (url) {
+        case '/about':
+        renderHTML('./about.html', res)
+            break;
+        case '/contact':
+            renderHTML('./contact.html', res)
+            break;
+        
+        default:
+            renderHTML('./index.html', res)
+            break;
     }
-    res.write('Hello World!');
-    res.end();
+
+    // if (url === '/about') {
+    //     renderHTML('./about.html', res)
+    // } else if (url === '/contact') {
+    //     renderHTML('./contact.html', res)
+    // } else {
+    //     renderHTML('./index.html', res)
+    // }
 }).listen(port, () => {
     console.log(`Server is listening on port ${port}`)
 })
